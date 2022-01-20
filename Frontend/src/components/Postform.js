60949967b2bfd34c55dcc008acea478d3749f66e
Catus1234 <89518980+Catus1234@users.noreply.github.com>
2022-01-20 12:19:36 +0800

    updated post form

--- Frontend/src/components/Postform.js
+++ Frontend/src/components/Postform.js
@@ -1,19 +1,31 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-export const Postform = () => {
-    const(form,setForm) = useState()
+export default function Postform(){
+    const[formData ,setFormData] = useState({
+        postTitle: "",
+        postDescription: "",
+        postImg: ""
+    }
+    )
  
- return 
-    <div>
+    function handleChange(event){
+        const {name, value} = event.target
+        setFormData(prevform => ({
+            ...prevform,
+            [name]: value
+        }))
+    }
+
+    return(
+        <div>
         <form>
-          <label>
-            <input type = "text" name ="Title of your post"/> 
-             <input type = "textarea" name ="Write your post"/> 
-             <input type = "image" name = "Upload image"/>
+        <label>
+            <input type = "text" name ="Title of your post" handleChange={handleChange}/> 
+            <input type = "textarea" name ="Write your post"/> 
+            <input type = "image" name = "Upload image"/>
             </label>
             <button type = "submit">Submit</button>
         </form>
-    </div>
-};
-
-export default Postform
+        </div>
+    )
+}
