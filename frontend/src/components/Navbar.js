b90fd311dbb5367811f4de7856e5e6cd402d05f8
Catus1234 <89518980+Catus1234@users.noreply.github.com>
2022-01-20 11:20:32 +0800

    added navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Navbar() {
     return (
         <nav>
-            
+            <div className="navbar--container">
+                <h2 className="navbar--proj">DBS SEED</h2>
+                <h3 className="navbar--home">Home</h3>
+                <h3 className="navbar--profile">Profile</h3>
+                <h3 className="navbar--logout">Logout</h3>
+            </div>
         </nav>
+        
     )
 }
