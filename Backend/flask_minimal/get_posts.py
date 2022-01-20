01e7612dd74cda135f9cf7a3502bdc55fff24bbf
jh-chee <jianhuichee97@gmail.com>
2022-01-20 13:20:34 +0800

    updated get post

--- Backend/flask_minimal/get_posts.py
+++ Backend/flask_minimal/get_posts.py
@@ -1,4 +1,3 @@
-
 import json
 from flask import Flask, jsonify, request
 
@@ -6,31 +5,48 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-with open('Data/POST.json', 'r') as myfile:
+
+with open("Data/POST.json", "r") as myfile:
     data = myfile.read()
 posts = json.loads(data)
 
 
-
-@app.route('/get_posts', methods=['GET'])
-def home():
-    return jsonify({'data': posts})
-
+@app.route("/get_posts", methods=["GET"])
+def get_posts():
+    return jsonify({"data": posts})
 
 
-@app.route('/get_post', methods=['GET'])
-def api_id():
-    if 'Post_ID' in request.args:
-        post_id = int(request.args['Post_ID'])
-    else:
-        return "Error: No id field provided. Please specify an id."
-
+@app.route("/get_post/<id>", methods=["GET"])
+def get_post_test(id):
     results = []
     for post in posts:
-        if post['Post_ID'] == post_id:
+        if post["Post_ID"] == int(id):
             results.append(post)
+    return jsonify({"data": results})
 
-    return jsonify(results)
+
+@app.route("/create_post", methods=["POST"])
+def create_post():
+    record = request.get_json()
+    posts.append(record)
+    return jsonify({"data": record})
 
 
-app.run()
+@app.route("/put_post", methods=["PUT"])
+def put_post():
+    record = request.get_json()
+    for i, post in enumerate(posts):
+        if post["Post_ID"] == record["Post_ID"]:
+            posts[i] = record
+    return jsonify({"data": record})
+
+
+@app.route("/delete_post/<id>", methods=["DELETE"])
+def delete_post(id):
+    for post in posts:
+        if post["Post_ID"] == int(id):
+            posts.remove(post)
+    return jsonify({"status": "deleted"})
+
+
+app.run(debug=True)
